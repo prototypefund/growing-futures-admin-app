2ca7493b3be7c79f628b75ff477dcad3a19cf393
Andreas Gmeiner <andreas@mission-five.io>
2019-11-07 10:13:18 +0100

    show name of referenced data in dropdown

--- src/components/db-admin/InputSingleSelect.vue
+++ src/components/db-admin/InputSingleSelect.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="input-single-select-container">
     <dropdown @change="update"
-              :preSelection="selection"
+              :preSelection="preSelection"
               :options="displayOptions"/>
   </div>
 </template>
@@ -10,6 +10,12 @@
 import Dropdown from '@/components/utils/Dropdown.vue'
 export default {
   name: 'InputSingleSelect',
+  created() {
+    let matchingOption = this.options.find(o => o.uid == this.selection) 
+    if (matchingOption){
+      this.preSelection = matchingOption.name
+    }
+  },
   props: {
     selection: String,
     options: Array,
@@ -20,20 +26,24 @@ export default {
   },
   data () {
     return {
+      preSelection: "",
       selectedOption: this.selection
     }
   },
   methods: {
     update(e) {
       let option = this.options.find(o => o.name == e)
-      this.$emit('update', e)
+      this.$emit('update', option.uid)
     }
   },
   computed: {
     displayOptions() {
       return this.options.map(o => o.name)
     }
-    
+  },
+  filters: {
+    displayName(options) {
+    }
   }
 }
 </script>
